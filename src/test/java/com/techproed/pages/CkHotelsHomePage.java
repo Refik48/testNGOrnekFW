4d6ca050829be097db2bdee76061dbe6fc0b4c53
Refik <rgulak48@gmail.com>
2020-12-24 21:42:50 +0200

    25.gun

--- src/test/java/com/techproed/pages/CkHotelsHomePage.java
+++ src/test/java/com/techproed/pages/CkHotelsHomePage.java
@@ -4,22 +4,18 @@ import com.techproed.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 public class CkHotelsHomePage {
-
     public CkHotelsHomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
-    @FindBy(linkText = "Log in")
-    public WebElement IlklogIn;
+    @FindBy(linkText="Log in")
+    public WebElement ilkLogIn;
     @FindBy(id="UserName")
     public WebElement userNameTextBox;
-
     @FindBy(id="Password")
     public WebElement passwordTextBox;
-
     @FindBy(id="btnSubmit")
-    public WebElement LoginButonu;
-
-
+    public WebElement loginButonu;
+    @FindBy(xpath = "//div[@class='validation-summary-errors']")
+    public WebElement girilemediYazisi;
 }
